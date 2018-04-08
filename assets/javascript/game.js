94b6e62444ab513dd10d978ffa1c74039304f591
five3183 <five3183@gmail.com>
2018-04-08 00:07:30 -0400

    update 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,87 +3,92 @@ $("#LOSSES").text(loss);
 var win = 0; 
 $("#WINS").text(win);
 var total = 0
+
+var redNumber = 0;
+var blueNumber = 0; 
+var yellowNumber = 0;
+var greenNumber = 0;
+
+var randomNumber = (Math.floor(Math.random()* 20) + 75);
+$(".randomBox").text(randomNumber);
+console.log("R1" + randomNumber)
+
+
 $(document).ready(function() {
-    $("#startGame").on("click", function newGame() {
+    $("#startGame").on("click", function Game() {
+        newGame();
+    });
+
+    function newGame() {
         total = 0;
 
         $(".SCORE").text(total);
-        var randomNumber = (Math.floor(Math.random()* 20) + 75);
+        randomNumber = (Math.floor(Math.random()* 20) + 75);
         $(".randomBox").text(randomNumber);
+        console.log("R2" + randomNumber)
         
         //Gem values
-        var redNumber = (Math.floor((Math.random() * 10) + 1));
-        var blueNumber = (Math.floor((Math.random() * 10) + 1));
-        var yellowNumber = (Math.floor((Math.random() * 10) + 1));
-        var greenNumber = (Math.floor((Math.random() * 10) + 1));
-    
-        //Click crystal generate random number
-        // $(".crystal").on("click", function() {
-        // });
+        redNumber = (Math.floor((Math.random() * 10) + 1));
+        blueNumber = (Math.floor((Math.random() * 10) + 1));
+        yellowNumber = (Math.floor((Math.random() * 10) + 1));
+        greenNumber = (Math.floor((Math.random() * 10) + 1));
+    }
 
-        $(".RED").on("click", function() {
-            console.log(redNumber);
-            total = total + redNumber;
-            $(".SCORE").text(total);
-                if(total === randomNumber) {
-                    winCon()
-                }
-                if(total > randomNumber) {
-                    lossCon()
-                }
-        });
-        $(".BLUE").on("click", function() {
-            console.log(blueNumber);
-            total = total + blueNumber;
-            $(".SCORE").text(total);
-                if(total === randomNumber) {
-                    winCon()
-                }
-                if(total > randomNumber) {
-                    lossCon()
-                }
-        });
-        $(".YELLOW").on("click", function() {
-            console.log(yellowNumber);
-            total = total + yellowNumber;
-            $(".SCORE").text(total);
-                if(total === randomNumber) {
-                    winCon()
-                }
-                if(total > randomNumber) {
-                    lossCon()
-                }
-        });
-        $(".GREEN").on("click", function() {
-            console.log(greenNumber);
-            total = total + greenNumber;
-            $(".SCORE").text(total);
-                if(total === randomNumber) {
-                    winCon()
-                }
-                if(total > randomNumber) {
-                    lossCon()
-                }
-        });
-        
-        function winCon() {
-            win++;
-            $("#WINS").text(win);
-            newGame();
-            return redNumber = 0;
-            return blueNumber = 0; 
-            return yellowNumber = 0;
-            return greenNumber = 0;
-        }
+    function winCon() {
+        win++;
+        $("#WINS").text(win);
+        newGame();
+    }
 
-        function lossCon() {
-            loss++;
-            $("#LOSSES").text(loss);
-            newGame();
-            return redNumber = 0;
-            return blueNumber = 0; 
-            return yellowNumber = 0;
-            return greenNumber = 0;
-        } 
-    })
+    function lossCon() {
+        loss++;
+        $("#LOSSES").text(loss);
+        newGame();
+    } 
+
+    $(".RED").on("click", function() {
+        console.log("redNumber: ", redNumber);
+        total = total + redNumber;
+        $(".SCORE").text(total);
+            if(total === randomNumber) {
+                winCon();
+            }
+            if(total > randomNumber) {
+                lossCon();
+            }
+    });
+    $(".BLUE").on("click", function() {
+        console.log("blueNumber: " + blueNumber);
+        total = total + blueNumber;
+        $(".SCORE").text(total);
+            if(total === randomNumber) {
+                winCon();
+            }
+            if(total > randomNumber) {
+                lossCon();
+            }
+    });
+    $(".YELLOW").on("click", function() {
+        console.log("yellowNumber: " + yellowNumber);
+        total = total + yellowNumber;
+        $(".SCORE").text(total);
+            if(total === randomNumber) {
+                winCon();
+            }
+            if(total > randomNumber) {
+                lossCon();
+            }
+    });
+    $(".GREEN").on("click", function() {
+        console.log("greenNumber: " + greenNumber);
+        total = total + greenNumber;
+        $(".SCORE").text(total);
+            if(total === randomNumber) {
+                winCon();
+            }
+            if(total > randomNumber) {
+                lossCon();
+            }
+    });
+
 });
